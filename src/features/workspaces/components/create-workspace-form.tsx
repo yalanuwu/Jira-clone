6a8c5cc2d05b40b70416274ec63c120b2c27472c
Abhigyan Mandal <iamabhigyan5253@gmail.com>
2024-11-25 13:23:09 +0530

    implemented image upload

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -4,6 +4,10 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { createWorkspaceSchema } from "../schemas";
 import { z } from "zod";
+import { useRef } from "react";
+import Image from "next/image";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { ImageIcon } from "lucide-react";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormLabel, FormMessage, FormItem, FormField } from "@/components/ui/form";
@@ -19,6 +23,8 @@ interface CreateWorkspaceFormProps {
 export const CreateWorkspaceForm = ({ onCancel } : CreateWorkspaceFormProps) => {
 
     const { mutate, isPending } = useCreateWorkspace();
+    const inputRef = useRef<HTMLInputElement>(null);
+
     const form = useForm<z.infer<typeof createWorkspaceSchema>>({
         resolver : zodResolver(createWorkspaceSchema),
         defaultValues: {
@@ -27,8 +33,24 @@ export const CreateWorkspaceForm = ({ onCancel } : CreateWorkspaceFormProps) => 
     });
 
     const onSubmit = (values : z.infer<typeof createWorkspaceSchema>) => {
-        mutate({ json : values });
+        const finalValues = {
+            ...values,
+            image: values.image instanceof File ? values.image : "",
+        }
+        mutate({ form : finalValues }, {
+            onSuccess: () => {
+                form.reset();
+                //TODO : REDIRECT TO NEW WORKSPACE
+            }
+        });
     };
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            form.setValue('image', file);
+        }
+    }
 
     return(
         <Card className="w-full h-full border-none shadow-none">
@@ -60,6 +82,61 @@ export const CreateWorkspaceForm = ({ onCancel } : CreateWorkspaceFormProps) => 
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
+                            )}
+                        />
+                        <FormField 
+                            control={form.control}
+                            name="image"
+                            render={({ field }) => (
+                                <div className="flex flex-col gap-y-2">
+                                    <div className="flex items-center gap-x-5">
+                                        {field.value ? (
+                                            <div className="size-[72px] relative rounded-md overflow-hidden">
+                                                <Image 
+                                                    src={
+                                                        field.value instanceof File
+                                                        ? URL.createObjectURL(field.value)
+                                                        : field.value
+                                                    }
+                                                    alt="logo"
+                                                    fill
+                                                    className="object-cover"
+                                                />
+                                            </div>
+                                        ) : (
+                                            <Avatar className="size-[72px]">
+                                                <AvatarFallback>
+                                                    <ImageIcon className="size-[36px] text-neutral-400"/>
+                                                </AvatarFallback>
+                                            </Avatar>
+                                        )}
+                                        <div className="flex flex-col">
+                                            <p className="text-sm">Workspace Icon</p>
+                                            <p className="text-sm text-muted-foreground ">
+                                                JPG, PNG, SVG or JPEG, max 1MB
+                                            </p>
+                                            <input 
+                                            className="hidden"
+                                            accept=".png, .jpg, .svg, .jpeg"
+                                            type="file"
+                                            ref={inputRef}
+                                            disabled={isPending}
+                                            onChange={handleImageChange}
+                                            />
+                                        </div>
+                                        <Button
+                                            type="button"
+                                            disabled={isPending}
+                                            variant={'tertiary'}
+                                            size={'xs'}
+                                            className="w-fit mt-2"
+                                            onClick={() => inputRef.current?.click()}
+                                        >
+                                            Upload Image
+                                        </Button>
+                                    </div>
+
+                                </div>
                             )}
                         />
                         </div>
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,19 +2,38 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, WORKSPACE_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACE_ID } from "@/config";
 import { ID } from "node-appwrite";
 
 const app = new Hono()
     .post(
         '/',
-        zValidator('json' , createWorkspaceSchema),
+        zValidator('form' , createWorkspaceSchema),
         sessionMiddleware,
         async (c) => {
             const databases = c.get('databases');
+            const storage = c.get('storage');
             const user = c.get('user');
 
-            const { name } = c.req.valid('json');
+            const { name, image } = c.req.valid('form');
+
+            let uploadedImageUrl : string | undefined;
+
+            if (image instanceof File) {
+                const file = await storage.createFile(
+                    IMAGES_BUCKET_ID,
+                    ID.unique(),
+                    image
+                );
+
+                const arrayBuffer = await storage.getFilePreview(
+                    IMAGES_BUCKET_ID,
+                    file.$id
+                )
+
+                uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString('base64')}`;
+
+            }
 
             const workspace = await databases.createDocument(
                 DATABASE_ID,
@@ -22,7 +41,8 @@ const app = new Hono()
                 ID.unique(),
                 {
                     name,
-                    userId : user.$id
+                    userId : user.$id,
+                    imageUrl : uploadedImageUrl,
                 }
             );
 
