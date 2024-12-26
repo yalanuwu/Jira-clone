4a2ddceab7b0f67908d7494734fc83d18e625c65
Abhigyan Mandal <iamabhigyan5253@gmail.com>
2024-12-26 11:52:55 +0530

    refactored server queries

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,25 +1,15 @@
 "use server";
 
-import { cookies } from "next/headers";
-import { Account, Client, Databases, Query } from "node-appwrite";
-import { AUTH_COOKIE } from "../auth/constants";
+import { Query } from "node-appwrite";
 import { DATABASE_ID, MEMBERS_ID, WORKSPACE_ID } from "@/config";
 import { getMember } from "../members/utils";
 import { Workspace } from "./types";
+import { createSessionClient } from "@/lib/appwrite";
 
 export const getWorkspaces = async () => {
     try {
-        const client = new Client()
-            .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-            .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-        const session = cookies().get(AUTH_COOKIE);
-
-        if (!session) return { documents : [], total : 0};
-        client.setSession(session.value);
-
-        const databases = new Databases(client);
-        const account = new Account(client);
+        
+        const { databases, account } = await createSessionClient();
         const user = await account.get();
 
         const members = await databases.listDocuments(
@@ -56,17 +46,8 @@ interface getWorkspaceProps {
 
 export const getWorkspace = async ({workspaceId} : getWorkspaceProps) => {
     try {
-        const client = new Client()
-            .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-            .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-        const session = cookies().get(AUTH_COOKIE);
-
-        if (!session) return null;
-        client.setSession(session.value);
-
-        const databases = new Databases(client);
-        const account = new Account(client);
+        
+        const { databases, account } = await createSessionClient();
         const user = await account.get();
 
         const member = await getMember({
