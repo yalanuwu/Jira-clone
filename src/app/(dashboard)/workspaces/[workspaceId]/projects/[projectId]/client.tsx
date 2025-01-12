af07ef80a7fa4bf7c63f528985b53272854f517d
Abhigyan Mandal <iamabhigyan5253@gmail.com>
2025-01-12 13:21:35 +0530

    created project analytics

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import Analytics from "@/components/analytics";
 import PageError from "@/components/page-error";
 import PageLoader from "@/components/page-loader";
 import { Button } from "@/components/ui/button";
 import { useGetProject } from "@/features/projects/api/use-getProject";
+import { useGetProjectAnalytics } from "@/features/projects/api/use-getProjectAnalytics";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { useProjectId } from "@/features/projects/hooks/use-projectId";
 import { TaskViewSwitcher } from "@/features/tasks/components/switcher";
@@ -13,13 +15,16 @@ import Link from "next/link";
 export const ProjectIdClient = () => {
 
     const projectId = useProjectId();
-    const {data, isLoading} = useGetProject({ projectId });
+    const {data : project , isLoading: isLoadingProject } = useGetProject({ projectId });
+    const { data: analytics , isLoading: isLoadingAnalytics } = useGetProjectAnalytics({ projectId });
+
+    const isLoading = isLoadingAnalytics || isLoadingProject;
 
     if(isLoading){
         return <PageLoader/>;
     }
 
-    if (!data){
+    if (!project){
         return <PageError message="Project not found" />
     }
 
@@ -28,21 +33,24 @@ export const ProjectIdClient = () => {
             <div className="flex items-center justify-between">
                 <div className="flex items-center gap-x-2">
                     <ProjectAvatar
-                        name={data.name}
-                        image={data.imageUrl}
+                        name={project.name}
+                        image={project.imageUrl}
                         className="size-8"
                     />
-                    <p className="font-semibold text-lg">{data.name}</p>
+                    <p className="font-semibold text-lg">{project.name}</p>
                 </div>
                 <div>
                     <Button variant={'secondary'} asChild size={'sm'}>
-                        <Link href={`/workspaces/${data.workspaceId}/projects/${data.$id}/settings`}>
+                        <Link href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}>
                             <PencilIcon className="size-4 mr-2"/>
                             Edit Project
                         </Link>
                     </Button>
                 </div>
             </div>
+            {analytics ? (
+                <Analytics data={analytics}/>
+            ) : null}
             <TaskViewSwitcher hideProjectFilter/>
         </div> 
     );
---
+++ src/components/analytics.tsx
@@ -0,0 +1,65 @@
+import { ProjectAnalyticsResponseType } from "@/features/projects/api/use-getProjectAnalytics";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
+import AnalyticsCard from "./analytics-card";
+import { dottedSeparator as DottedSeparator } from "./dotted-line-separator";
+
+const Analytics = ({ data } : ProjectAnalyticsResponseType) => {
+
+    if (!data){
+        return null;
+    }
+    return ( 
+        <ScrollArea className="border rounded-lg w-full whitespace-nowrap shrink-0">
+            <div className="flex flex-row w-full">
+                <div className="flex items-center flex-1">
+                    <AnalyticsCard 
+                        title="Total Tasks"
+                        value={data.taskCount}
+                        variant={data.taskDifference > 0 ? 'up' : 'down'}
+                        increaseValue={data.taskDifference}
+                    />
+                    <DottedSeparator direction="vertical" />
+                </div>
+                <div className="flex items-center flex-1">
+                    <AnalyticsCard 
+                        title="Assigned Tasks"
+                        value={data.assignedTaskCount}
+                        variant={data.assignedTaskDifference > 0 ? 'up' : 'down'}
+                        increaseValue={data.assignedTaskDifference}
+                    />
+                    <DottedSeparator direction="vertical" />
+                </div>
+                <div className="flex items-center flex-1">
+                    <AnalyticsCard 
+                        title="Completed Tasks"
+                        value={data.completedTaskCount}
+                        variant={data.completedTaskDifference > 0 ? 'up' : 'down'}
+                        increaseValue={data.completedTaskDifference}
+                    />
+                    <DottedSeparator direction="vertical" />
+                </div>
+                <div className="flex items-center flex-1">
+                    <AnalyticsCard 
+                        title="Overdue Tasks"
+                        value={data.overdueTaskCount}
+                        variant={data.overdueTaskDifference > 0 ? 'up' : 'down'}
+                        increaseValue={data.overdueTaskDifference}
+                    />
+                    <DottedSeparator direction="vertical" />
+                </div>
+                <div className="flex items-center flex-1">
+                    <AnalyticsCard 
+                        title="Incomplete Tasks"
+                        value={data.incompleteTaskCount}
+                        variant={data.incompleteTaskDifference > 0 ? 'up' : 'down'}
+                        increaseValue={data.incompleteTaskDifference}
+                    />
+                    <DottedSeparator direction="vertical" />
+                </div>
+            </div>
+            <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+    );
+}
+ 
+export default Analytics;
